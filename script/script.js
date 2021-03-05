a4fc509e082be2f6abe4be0a4dbb814fbec73377
valek-web <59340767+valek-web@users.noreply.github.com>
2021-03-05 17:56:26 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,23 @@
+let open_menu = () => {
+    document.querySelector('nav').style.display = 'block';
+    document.querySelector('.close').style.display = 'block';
+    document.querySelector('.background').style.display = 'block';
+}
+
+let close_menu = () => {
+    document.querySelector('nav').style.display = 'none';
+    document.querySelector('.close').style.display = 'none';
+    document.querySelector('.background').style.display = 'none';
+}
+
+let noScrollSection = () => {
+    document.querySelector('body').style.overflow = 'hidden'
+}
+
+let yesScrollSection = () => {
+    document.querySelector('body').style.overflow = 'auto'
+}
+
+
+document.querySelector('nav').addEventListener('mouseover', noScrollSection)
+document.querySelector('section').addEventListener('mouseover', yesScrollSection)
